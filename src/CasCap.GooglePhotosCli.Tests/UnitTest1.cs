36bcea5d4c33edb44f21e8cd0b03143db70382e5
Alex Vincent <alexv@microsoft.com>
2020-11-20 16:59:56 +0100

    add shell unit test project

---
+++ src/CasCap.GooglePhotosCli.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+namespace CasCap.GooglePhotosCli.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //todo: how best to test a command line application?
+            Assert.True(true);
+        }
+    }
+}
